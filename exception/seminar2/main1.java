1460402d9f9058f8a23d81fce2eebcca3fefcc5a
Alexey <alex3122007@yandex.ru>
2023-07-29 13:29:01 +0300

    homeworkE2

---
+++ exception/seminar2/main1.java
@@ -0,0 +1,23 @@
+package OOP.exception.seminar2;
+
+import java.util.Scanner;
+//                                              Задание 1
+public class main1 {
+    public static void main(String[] args) {
+        System.out.println(getFloat());
+    }
+
+    static float getFloat() {
+        Scanner scanner = new Scanner(System.in);
+        float i;
+        System.out.println("Введите дробное число: ");
+        if (scanner.hasNextFloat()) {
+            i = scanner.nextFloat();
+        } else {
+            System.out.print("Ошибка, ");
+            scanner.next();
+            i = getFloat();
+        }
+        return i;
+    }
+}
---
+++ exception/seminar2/main4.java
@@ -0,0 +1,24 @@
+package OOP.exception.seminar2;
+
+import java.util.Scanner;
+
+public class main4 {
+    public static void main(String[] args) {
+
+        getString();
+      
+        System.out.println("end");
+    }
+
+    static void getString() {
+        Scanner scanner = new Scanner(System.in);
+        String s = null;
+        s = scanner.nextLine();
+        if (s.equals("")) {
+            throw new IllegalArgumentException("Пустые строки вводить нельзя");
+        }
+        System.out.println(s.length());
+
+    }
+
+}
